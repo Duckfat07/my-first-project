db4293abae969062c5a85b0c17497c06aec58535
Ethan Kim <kim.ethanj@gmail.com>
2026-02-12 13:31:11 -0500

    added dice game

---
+++ dicegame.py
@@ -0,0 +1,22 @@
+import random
+playing = True
+while playing:
+    choice = input('Roll the dice? (y/n):').lower()
+    if choice == 'y' or choice == 'Y':
+        die1= random.randint(1,6)
+        die2 = random.randint(1,6)
+        print(f'({die1},{die2})')
+    elif choice == 'n':
+        print('Thanks for playing!')
+        break
+    else:
+        print('Invalid choice!')
+#Ask: roll the dice?
+# If user enters y
+#   Generate two random numbers
+#   Print them
+# If user enters n
+#   Print thank you message
+#   Terminate
+# Else
+#   Print invalid choice
