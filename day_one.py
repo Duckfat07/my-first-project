89768f1269e4d54a83124b8d09d161f993d6399f
Ethan Kim <kim.ethanj@gmail.com>
2026-02-13 23:06:34 -0500

    day_one python learning repository

---
+++ day_one.py
@@ -0,0 +1,22 @@
+print("Hello World!") #print hello world
+
+print(2+3) # addition(+)
+print(10-5) # subtraction (-)
+print(6*3) # multiplication (*)
+print(32/8) # division (/)
+
+print(9%5) # modulus operator (returns the remainder after one number is divided by another)
+print(3**3) # exponential (**)
+print(5//4) # floor division operator, integer division operator (//, performs division and rounds the result down to the nearest whole number and discards the fractional part of the result)
+
+# Checking data types
+
+print(type(10)) # Int
+print(type(3.14)) # Float
+print(type(1+3j)) # complex (imaginary numbers, doesn't use i because it conflicts with syntax)
+print(type('Ethan')) # String
+print(type([1, 2, 3])) # List
+print(type({'name': 'Ethan Kim'})) # Dictionary
+print(type({9.8, 3.14, 2.7})) # Tuple
+
+
