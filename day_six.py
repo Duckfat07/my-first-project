3d6037d684b79b35c99880d737781639edb749b8
Ethan Kim <kim.ethanj@gmail.com>
2026-02-25 10:56:22 -0500

    tuples finished

--- day_six.py
+++ day_six.py
@@ -66,4 +66,71 @@ fruits[2] = 'strawberry' # changes the third item in the list to 'strawberry'
 print(fruits) # this will print the modified list
 
 # checking an item in a tuple
+tpl = ('item1', 'item2', 'item3')
+'item2' in tpl # True, because 'item2' is in the tuple
+print('item2' in tpl) # this will print True
 
+colors = ('red', 'green', 'blue')
+print('red' in colors) # this will print True
+print('black' in colors) # this will print False
+
+# Joining Tuples
+tpl1 = ('one', 'two', 'three')
+tpl2 = ('four', 'five', 'six')
+joined_tuple = tpl1 + tpl2 # this will join the two tuples together
+print(joined_tuple) # this will print the joined tuple
+
+foods = ('pasta', 'burger', 'sushi')
+desserts = ('brownie', 'pudding', 'cake')
+foods_and_desserts = foods + desserts # this will join the two tuples together
+print(foods_and_desserts) # this will print the joined tuple
+
+# Deleting a tuple
+tuple1 = ('joe', 'john', 'jane')
+del tuple1 # this will delete the tuple
+# print(tuple1) # this will raise an error because the tuple has been deleted
+citrus = ('lemon', 'orange', 'grapefruit')
+# del citrus[0] # this will raise an error because tuples are immutable and we cannot change them
+del citrus # this will delete the tuple
+# print(citrus) # this will raise an error because the tuple has been deleted
+
+# Exercise 1
+brothers = ('jim', 'jack', 'john')
+sisters = ('jane', 'jill', 'jessica')
+siblings = brothers + sisters 
+print(siblings) 
+print(len(siblings)) 
+parents = ('michael', 'susan')
+# join tuples
+family_members = siblings + parents
+print(family_members) 
+
+# Exercise 2
+# Unpacking a tuple 
+a, b, *others = family_members
+print(a, b, *others) # this will print the first two items in the tuple and the rest of the items will be stored in the 'others' variable as a list
+
+# tuple methods
+fruits = ('apple', 'blueberry','lime')
+vegetables = ('lettuce', 'broccoli', 'spinach')
+animal_products = ('beef', 'eggs', 'milk')
+# Join
+food_stuff_tp = fruits + vegetables + animal_products
+print(food_stuff_tp)
+food_stuff_lt = list(food_stuff_tp) # convert tuple to list
+print(food_stuff_lt)
+# slicing
+middle_item = food_stuff_lt[4] 
+print(middle_item) # this will print the middle item
+first_three_items = food_stuff_lt[0:3] 
+last_three_items = food_stuff_lt[-3:] 
+print('first three items:', first_three_items)
+print('last three items:', last_three_items)
+del food_stuff_tp 
+
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+print('Estonia' in nordic_countries)
+print('Iceland' in nordic_countries) 
+
+food_stuff_lt.sort()
+print(food_stuff_lt)
