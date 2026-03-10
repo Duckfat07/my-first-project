95940da603bead1aef5c5d62dc0b1038a546797d
Ethan Kim <kim.ethanj@gmail.com>
2026-03-10 11:09:28 -0400

    while loops and for loops

--- day_ten.py
+++ day_ten.py
@@ -52,3 +52,49 @@ while number < 5:
     count = count + 1
 # the above while loop only prints 0, 1, 2, and 4 (skips 3)
 
+# for loops 
+# syntax
+# for iterator in lst:
+#   code goes here
+
+# example
+numbers = [0, 1, 2, 3, 4, 5]
+for number in numbers: # number is temporary name to refer to the list's items, valid only inside this loop
+    print(number) # the numbers will be printed line by line, from 0 to 5
+
+# using For loop on string
+# for iterator in string:
+#   code goes here
+language = 'Python'
+for letter in language: 
+    print(letter)
+
+for i in range(len(language)):
+    print(language[i])
+
+# Using For loop on tuple
+# for iterator in tpl:
+#   code goes here
+numbers = (0,1,2,3,4,5)
+for number in numbers:
+    print(number)
+# For loop with dictionary 
+# for iterator in dct:
+#   code goes here
+soccer_player = {
+    'first_name':'Michael',
+    'last_name':'Parks',
+    'age': 85,
+    'country':'Estonia',
+    'is_married': True,
+    'skills':['crossing','shooting','dribbling'],
+    'address':{
+        'street':'Van Nostrand',
+        'zipcode':'07650'
+    }
+}
+for key in soccer_player:
+    print(key)
+for key, value in soccer_player.items():
+    print(key, value) # this way we get both keys and values printed out
+# example output: first_name Michael
