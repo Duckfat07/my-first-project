7dc8ede4b4dc82dc324633be10f8df4646cee9b4
Ethan Kim <kim.ethanj@gmail.com>
2026-04-01 10:20:54 -0400

    loops continued.

--- day_ten.py
+++ day_ten.py
@@ -119,3 +119,62 @@ for number in numbers:
     if number == 3:
         break
 # the loop stops when it reaches 3
+
+# Continue: we use continue when we want to skip some of the steps in the iteration of the loop
+# for iterator in sequence:
+#   code goes here
+#   if condition:
+#       continue
+numeros = (10,11,12,13,14,15)
+for numero in numeros:
+    print(numero)
+    if numero == 13:
+        continue
+    print('Next numero should be ', numero + 1) if numero != 5 else print('outside the loop')
+# if the number equals 13, the step after the condition (but inside the loop) is skipped and the execution of the loop continues if there are any iterations left
+
+# The range function
+# The range() function is used to return a list of numbers. The range(start, end, step) takes three parameters: starting, ending, and increment. By default it starts from 0 and the increment is 1. The range sequence needs at least 1 argument (end). Creating sequences using range
+lst = list(range(11)) 
+print(lst) # prints numbers from 0 to 10
+st = set(range(1, 11)) # 2 arguments indicate start and end of the sequence, step set to default 1
+print(st) # prints numbers from 1 to 10 
+
+lst = list(range(0, 11, 3)) # 3 arguments indicate start, end, and step of the sequence
+print(lst) # prints 0,3,6,9 
+st = set(range(1, 11, 2)) 
+print(st) # {1, 3, 5, 7, 9}
+
+# for backward from start to end
+lst = list(range(11, 0, -1)) 
+print(lst) # [11, 9, 7, 5, 3, 1]
+
+# syntax
+# for iterator in range(start, end, step):
+
+# for number in range(11):
+#     print(number) # prints from 0 to 10, not including 11
+
+
+# Nested For Loop
+# syntax
+# for x in y:
+#     for t in x:
+#         code goes here
+
+person = {
+    'name':'johnny',
+    'age':25,
+    'hobbies':['hiking', 'cooking', 'soccer'],
+    'is_married': False,
+    'address':{
+        'avenue':'Columbus',
+        'zipcode':'11111'
+    }
+}
+for key in person:
+    if key == 'hobbies':
+        for hobby in person[key]:
+            print(hobby) # prints hobbies one by one
+
+# For Else
